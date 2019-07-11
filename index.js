4e0a7a18d529133753a6074706d94670e7299e0b
Felipe Amaral de Oliveira <felipeamaral83@yahoo.com.br>
2019-07-10 23:23:49 -0300

    Implementação do redux.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import {AppRegistry} from 'react-native';
-import Navigator from './src/Navigator';
-import {name as appName} from './app.json';
+import React from 'react'
+import { YellowBox } from 'react-native'
+import { Provider } from 'react-redux'
+import { AppRegistry } from 'react-native'
+import Navigator from './src/Navigator'
+import { name as appName } from './app.json'
+import storeConfig from './src/store/storeConfig'
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const store = storeConfig()
+
+const Redux = () => (
+    <Provider store={store}>
+        <Navigator />
+    </Provider>
+)
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']) 
+AppRegistry.registerComponent(appName, () => Redux)
--- src/Navigator.js
+++ src/Navigator.js
@@ -1,9 +1,36 @@
 import React from 'react'
-import { createBottomTabNavigator } from 'react-navigation'
+import {
+    createBottomTabNavigator,
+    createSwitchNavigator,
+    createStackNavigator
+} from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
+import Profile from './screens/Profile'
+import Login from './screens/Login'
+import Register from './screens/Register'
+
+const authRouter = createStackNavigator({
+    Login: {
+        screen: Login,
+        navigationOptions: { title: 'Login' }
+    },
+    Register: {
+        screen: Register,
+        navigationOptions: { title: 'Registrar' }
+    }
+}, {
+    initialRouteName: 'Login'
+})
+
+const loginOrProfileRouter = createSwitchNavigator({
+    Profile: Profile,
+    Auth: authRouter
+}, {
+    initialRouteName: 'Auth'
+})
 
 const MenuRoutes = {
     Feed: {
@@ -26,7 +53,7 @@ const MenuRoutes = {
     },
     Profile: {
         name: 'Profile',
-        screen: Feed,
+        screen: loginOrProfileRouter,
         navigationOptions: {
             title: 'Profile',
             tabBarIcon: ({ tintColor }) =>
